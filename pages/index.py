3da15535fcb232a3d1ae4936404244b59246749a
MaxTechniche <Max.Techniche@gmail.com>
2020-11-18 11:47:08 -0700

    Added FrontPage

--- pages/index.py
+++ pages/index.py
@@ -1,4 +1,3 @@
-# Imports from 3rd party libraries
 import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
@@ -6,30 +5,34 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
 
-# Imports from this application
 from app import app
+from graphing3d import fig
 
-# https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
-layout = html.Div(
+column1 = dbc.Col(
     [
         dcc.Markdown(
             """
         
-            ## Honey! I'm HOOOMMMMEE!
+            ## Welcome to Spot The Music!
+
+            Seen here is approximately a 3% sample of a database found at [Kaggle.com](https://www.kaggle.com/yamaerenay/spotify-dataset-19212020-160k-tracks).  
+
+            This graph has 5000 randomly selected songs from the database of over 160 thousand songs.  
+
+            Click the button below and hopefully you can find a new song you like.
 
             """
         ),
-        dcc.Link(dbc.Button('Find New Songs', color='primary'), href='/predictions')
+        dcc.Link(dbc.Button('Discover New Songs', color='primary'), href='/predictions')
+    ], md=4
+)
+
+
+
+graph = dbc.Col(
+    [
+        dcc.Graph(figure=fig),
     ]
 )
-# gapminder = px.data.gapminder()
-# fig = px.scatter(gapminder.query("year==2007"), x="gdpPercap", y="lifeExp", size="pop", color="continent",
-#            hover_name="country", log_x=True, size_max=60)
 
-# column2 = dbc.Col(
-#     [
-#         dcc.Graph(figure=fig),
-#     ]
-# )
-
-# layout = dbc.Row([column1, column2])
+layout = dbc.Row([column1, graph])

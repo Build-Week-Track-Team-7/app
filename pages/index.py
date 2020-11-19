ef46d7384cc99bb5bb4525b3c2baf1bbd9487d55
MaxTechniche <Max.Techniche@gmail.com>
2020-11-19 16:20:08 -0700

    not much

--- pages/index.py
+++ pages/index.py
@@ -14,18 +14,31 @@ column1 = dbc.Col(
         dcc.Markdown(
             """
         
-            ## Welcome to Spot The Music!
+            ## Welcome to  
+            # Spot The Music!
 
-            Seen here is approximately a 3% sample of a database found at [Kaggle.com](https://www.kaggle.com/yamaerenay/spotify-dataset-19212020-160k-tracks).  
 
-            This graph has 5000 randomly selected songs from the database of over 160 thousand songs.  
+            This graph shows the relation between a subset of features from a total data set.  The features displayed are the 3 most important features to the machine learning model.  This relation shows the clustering and which pieces of music are most similar to each other based on the total available 15 features. The different colors indicate which group, after applying a K-means test to the data, the individual song belongs to.  You will notice that as you hover over a point you will see the song title it’s group number and the values of each feature.  The 3 predominant features that are graphed are ‘energy’ ‘danceability’ and ‘liveness’.  
+            
+
+            Energy - refers to the perceptual measure of intensity and activity. Typically, energetic tracks feel fast, loud, and noisy. For example, death metal has high energy, while a Bach prelude scores low on the scale. Perceptual features contributing to this attribute include dynamic range, perceived loudness, timbre, onset rate, and general entropy.  
+            
+            
+            Danceability - this is on a scale of 0-1 of how easy a song is to dance too based on various features.  
+            
+            
+            Liveness - is a feature that describes how detectable a live audience is in a recording. 
+
+            We use these features along with several others to see if any songs have similarities and can provide a similar experience to the user.  
 
             Click the button below and hopefully you can find a new song you like.
+            
+            Dataset used for this project can be found at [Kaggle.com](https://www.kaggle.com/yamaerenay/spotify-dataset-19212020-160k-tracks).  
 
             """
         ),
         dcc.Link(dbc.Button('Discover New Songs', color='primary'), href='/predictions')
-    ], md=4
+    ], width=4
 )
 
 
@@ -33,7 +46,7 @@ column1 = dbc.Col(
 graph = dbc.Col(
     [
         dcc.Graph(figure=Antony.plotlyfromjson('frontpagefig.json')),
-    ]
+    ], width=6
 )
 
 layout = dbc.Row([column1, graph])
--- pages/insights.py
+++ pages/insights.py
@@ -7,19 +7,33 @@ from dash.dependencies import Input, Output
 
 from app import app
 
+image_descriptions = {
+    'kmeans_pairplots.png': """The pair plots shown here show how the various features are related to one another as a part of the whole relationship that allows us to group various songs together.  You can see in many of the graphs some of the features have more strongly similar clusters then others in relation to one another.  These clustering patterns indicate the different groups and their correlated values.  These correlated values then determine the grouping that the various songs would belong to indicating what would be a similar experience for the user.""",
+
+    
+}
+
+
 insight_header = dbc.Col([
         dcc.Markdown(
             """
             ## Insights
             """),
         html.Hr(),
-        html.Img(src='assets/img/kmeans_pairplots.png', style={'width': '100%'})
-
     ])
 
-insights = dbc.Col([
-    
-])
+
+insights = []
+
+for image, description in image_descriptions.items():
+    insights.extend([
+        html.A(html.Img(src=f'assets/img/{image}', style={'width': '90%'}), href=f'https://raw.githubusercontent.com/Build-Week-Track-Team-7/app/main/assets/img/{image}'),
+        dcc.Markdown(f'{description}', style={'margin-left': '10px'})
+    ])
 
 
-layout = [insight_header]
+
+insights = dbc.Col(insights)
+
+
+layout = [insight_header, insights]
